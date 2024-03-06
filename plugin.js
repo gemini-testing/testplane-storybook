1ea9f43b1dc99a218fdb994a6985da65ee0a144e
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2024-03-06 17:11:14 +0300

    chore: purge everything

--- plugin.js
+++ plugin.js
@@ -1,1 +1,10 @@
-module.exports = require("./build/hermione");
+"use strict";
+
+module.exports = () => {
+    const errorMessage = [
+        "hermione-storybook: the functionality of the plugin was significantly changed",
+        "Continue using v0 or read about new hermione-storybook at https://github.com/gemini-testing/hermione-storybook"
+    ].join("\n");
+
+    throw new Error(errorMessage);
+};
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import HermioneDecorator from "./addon/decorators/hermione";
-import type { MakeDecoratorResult } from "@storybook/addons";
-
-export * from "./types";
-export const withHermione = (): MakeDecoratorResult => new HermioneDecorator().make();
